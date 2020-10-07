f851ecd481cdb0ccf68b5ba009572fcb71b6be2e
Hilmi Hilmi <hilmi.kyosou.080202@gmail.com>
2020-10-07 11:35:10 +0700

    11.34

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,27 +4,30 @@
     <div class="navbarr"> <img alt="EKASBON" id="navbar-image" src="../assets/logo.png"></div> 
     
     <button @click="getObject()">GET Object</button>
+    {{datas}}
+    <p></p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Parse from 'parse'
-
+let datas = []
 export default {
   name: 'Home',
   data() {
     return {
-
+      datas: datas,
     }
   },
   methods: {
     getObject() {
       let query = new Parse.Query("Users");
-      //query.equalTo("name", "hilmi");
+      //query.equalTo("Nama_Lengkap", "Adudu");
       query.find().then(
         (result) => {
          console.log('ParseObject detected. Showing result', result);
+         this.datas = result;
         },
         (error) => {
           console.log('Error while trying to show ParseObject: ', error);
